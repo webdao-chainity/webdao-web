7f3ac4eb530e0a63273f0dfe7292c44623fb9a58
lone wolf <leviethung2403@gmail.com>
2022-11-26 05:54:47 +0700

    fix base url of api

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ WORKDIR /app
 
 COPY . /app/
 
+ENV NEXT_PUBLIC_CHAIN_ID 5
+ENV NEXT_PUBLIC_NODE_1 https://goerli.infura.io/v3/
+ENV NEXT_PUBLIC_NODE_2 https://rpc.ankr.com/eth_goerli
+ENV NEXT_PUBLIC_NODE_3 https://eth-goerli.public.blastapi.io/
+ENV NEXT_PUBLIC_API_URL https://beta-api.webdao.app/
+
 RUN yarn install
 
 RUN yarn run build
--- constants/env.tsx
+++ constants/env.tsx
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import voteAbi from '@/constants/voteContractAbi.json';
 
-export const BASE_URL = _.get(process, '.env.NEXT_PUBLIC_API_URL') || 'http://154.26.134.145:5555/';
+export const BASE_URL =
+  _.get(process, '.env.NEXT_PUBLIC_API_URL') || 'https://beta-api.webdao.app/';
 
 export const VOTE_CONTRACT_ADDRESS =
   _.get(process, '.env.NEXT_PUBLIC_VOTE_ADDRESS') || '0x78B534F2B83863C21FabE5447e7af4ee71ddfC55';
